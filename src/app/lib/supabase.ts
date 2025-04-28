670ab4d4119fdbef1e2f85ffff7293bfbcb311a7
sd-codebase <krishna.dhas021815@gmail.com>
2025-04-28 20:00:20 +0530

    Update supabase.ts

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -2,9 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for server-side operations
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  process.env.SUPABASE_SERVICE_ROLE_KEY ||
-  "";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
